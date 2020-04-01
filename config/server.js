b1838369bb7c24aaa03a30ecf2e3f889be38430c
rahuljindal18 <rahuljindal1989@gmail.com>
2020-04-01 15:57:26 +0530

    react-infinite-scroll

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APPLICATION_ID:
+    process.env.APPLICATION_ID || "OpUWx5mwAk_LfQChLWgkuq2mB29ywoE0pXdxz_9rMkQ",
+  SECRET: process.env.SECRET || "oV4l5qFVstOMifTx4DS6D5IoVg31_H5lkrv_uLtPPXY",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
